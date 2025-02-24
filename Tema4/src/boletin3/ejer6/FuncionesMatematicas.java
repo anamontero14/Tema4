7c8fa12c759e83e873d48d53d9435906125f7d5d
USUARIO <USUARIO@DESKTOP-SOMRI2N>
2025-02-24 13:10:42 +0100

    Boletín 3 terminado

--- Tema4/src/boletin3/ejer6/FuncionesMatematicas.java
+++ Tema4/src/boletin3/ejer6/FuncionesMatematicas.java
@@ -20,13 +20,40 @@ public class FuncionesMatematicas {
 		return resultado;
 	}
 
+	/**
+	 * Devuelve el resultado de a^n
+	 * 
+	 * @param a
+	 * @param n exponente
+	 * @return
+	 */
 	public static double potencia(double a, int n) {
 		double resultado = 0;
 
-		if (a == 1) {
-			resultado += 1;
+		if (n == 0) {
+			resultado = 1;
 		} else {
-			resultado += a + potencia(a - 1, n);
+			resultado = a * potencia(a, n - 1);
+		}
+
+		return resultado;
+	}
+
+	/**
+	 * Esta función se le pasará un número entero y nos devolverá el término
+	 * correspondiente de la serie de Fibonacci. El enésimo valor de la serie de
+	 * Fibonacci se calcula sumando los dos valores anteriores de la serie
+	 * 
+	 * @param numero
+	 * @return
+	 */
+	public static int serieFibonacci(int numero) {
+		int resultado = 0;
+
+		if (numero == 0 || numero == 1) {
+			resultado = 1;
+		} else {
+			resultado = serieFibonacci(numero - 1) + serieFibonacci(numero - 2);
 		}
 
 		return resultado;
--- Tema4/src/boletin3/ejer6/PrincipalFuncionesMatematicas.java
+++ Tema4/src/boletin3/ejer6/PrincipalFuncionesMatematicas.java
@@ -12,15 +12,34 @@ public class PrincipalFuncionesMatematicas {
 		// variable para almacenar hasta qué nº se quiere sumar
 		int numero;
 
+		// variable para almacenar el número que se elevará
+		int numeroAElevar;
+
+		// variable para almacenar el exponente
+		int exponente;
+
+		// variable donde se almacenara el numero para la serie fibonacci
+		int numeroFibonacci;
+
 		System.out.print("Introduzca hasta qué nº quiere sumar: ");
 		numero = leer.nextInt();
 
 		System.out.println(FuncionesMatematicas.sumatorio(numero));
-		
+
 		System.out.println();
 		System.out.println();
-		
-		
+
+		System.out.print("Introduzca el nº que quiere elevar: ");
+		numeroAElevar = leer.nextInt();
+		System.out.print("Introduzca el exponente:");
+		exponente = leer.nextInt();
+		System.out.println(FuncionesMatematicas.potencia(numeroAElevar, exponente));
+
+		System.out.println();
+		System.out.println();
+		System.out.print("Introduzca el número del que quiere hacer la serie Fibonacci: ");
+		numeroFibonacci = leer.nextInt();
+		System.out.println(FuncionesMatematicas.serieFibonacci(numeroFibonacci));
 
 		// cerrar scanner
 		leer.close();
